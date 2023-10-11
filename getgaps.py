6308f589bd7a6be22ba4f9ef3d451a8033a26a23
Xu Wang <89448414+Immortal2333@users.noreply.github.com>
2023-10-11 15:57:50 +0800

    Add files via upload

---
+++ getgaps.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+'''
+@File    :   getgaps.py
+@Time    :   2029/09/01
+@Author  :   Xu Wang
+@Version :   1.0
+@Contact :   wangxu2018@cau.edu.cn
+@License :   (C)Copyright 2021-2022, CAAS ShenZhen
+@Desc    :   Get the information of gaps with gff format
+'''
+import argparse
+import re
+from Bio import SeqIO
+
+# Parse command-line arguments
+parser = argparse.ArgumentParser()
+parser.add_argument("fasta")
+args = parser.parse_args()
+
+# Open FASTA, search for masked regions, print in GFF3 format
+with open(args.fasta) as handle:
+    i = 0
+    for record in SeqIO.parse(handle, "fasta"):
+        for match in re.finditer('N+', str(record.seq)):
+            i = i+1
+            print (record.id, ".", "gap", match.start() + 1, match.end(), ".", ".", ".", "Name=gap" + str(i) + ";size=" + str(match.end()-match.start()), sep='\t')
+
+#use the following at CMD: FILENAME.py FILENAME.fasta >> FILENAME.gff3  here
